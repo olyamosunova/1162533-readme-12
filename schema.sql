7b431a67464b76acb63c7c4e111fdf0a298173b1
olya-mosunova <o.mosunova@skillline.ru>
2021-04-05 18:43:29 +0200

    Правит наименование таблиц и полей

--- schema.sql
+++ schema.sql
@@ -2,37 +2,34 @@ CREATE DATABASE readme
     DEFAULT CHARACTER SET utf8
     DEFAULT COLLATE utf8_general_ci;
 
-USE readme;
-
-CREATE TABLE users (
+CREATE TABLE user (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    data_add TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    date_add TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     email VARCHAR(128) NOT NULL UNIQUE,
     login VARCHAR(128),
     password CHAR(64) NOT NULL,
     avatar TEXT
 );
 
-CREATE TABLE posts (
+CREATE TABLE post (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    data_add TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    date_add TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     title TEXT,
     content TEXT,
-    AUTHOR VARCHAR(128),
+    author VARCHAR(128),
     image TEXT,
     video TEXT,
     link TEXT,
     shown_count INT(11),
-    author_id INT(11),
-    type_content_id INT(11),
-    hashtags INT(11)
+    user_id INT(11),
+    content_type_id INT(11)
 );
 
-CREATE TABLE comments (
+CREATE TABLE comment (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    data_add TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    date_add TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     message TEXT,
-    author_id INT(11),
+    user_id INT(11),
     post_id INT(11)
 );
 
@@ -42,21 +39,21 @@ CREATE TABLE likes (
     post_id INT(11)
 );
 
-CREATE TABLE subscriptions (
+CREATE TABLE subscription (
     id INT AUTO_INCREMENT PRIMARY KEY,
     user_id INT(11),
     follower_id INT(11)
 );
 
-CREATE TABLE messages (
+CREATE TABLE message (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    data_add TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    date_add TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     content TEXT,
-    author_id INT(11),
+    user_id INT(11),
     recipient_id INT(11)
 );
 
-CREATE TABLE hashtags (
+CREATE TABLE hashtag (
     id INT AUTO_INCREMENT PRIMARY KEY,
     title varchar(128) UNIQUE
 );
@@ -65,4 +62,9 @@ CREATE TABLE content_type (
     id INT AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(128) UNIQUE,
     class_name VARCHAR(128) UNIQUE
+);
+
+CREATE TABLE PostHashtag (
+    post_id INT(11),
+    hashtag_id INT(11)
 );
